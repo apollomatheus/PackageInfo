cc7c88f78c1921fe018331c8ecf819133f8ca01c
apollomatheus <matheuslds1@gmail.com>
2019-09-21 19:17:15 -0300

    'test'

---
+++ index.js
@@ -0,0 +1,36 @@
+
+function(()=>{
+  const extension = '.vue'; 
+  const plugin = new Plugin({
+    name: "PluginTest"
+  });
+  
+  const checkFileName = function () {
+    const filename = filepath;
+    var f = filename?filename.length>0?filename.split('\\'):null:null;
+    if (f && f.length > 0) {
+      f = f[f.length-1];
+      if (f.split(extension).length > 1) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  const formatCode = function () {
+      if (!checkFileName()) return;
+      fs.readFile(filepath,
+          "utf8",
+          function(err, data) {
+              console.log(data);
+          }
+      });
+  } 
+
+  async function loopTest(){
+      const delay = setTimeout(() => {
+        formatCode();
+      },1000);
+    loopTest();
+  })
+}());
